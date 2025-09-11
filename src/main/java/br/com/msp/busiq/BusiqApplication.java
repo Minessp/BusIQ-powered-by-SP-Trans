66f5ada76c2536461143893ae4d58b5fa92176f7
Minessp <minestadionchannel@gmail.com>
2025-09-11 17:45:58 -0300

    feat: Added Spring Boot Files

---
+++ src/main/java/br/com/msp/busiq/BusiqApplication.java
@@ -0,0 +1,13 @@
+package br.com.msp.busiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msp/busiq/BusiqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msp.busiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
