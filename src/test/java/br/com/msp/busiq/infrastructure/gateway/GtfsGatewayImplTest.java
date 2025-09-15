7e760ecccfe83a6befef6386a761c19a47b8a737
Minessp <minestadionchannel@gmail.com>
2025-09-15 18:39:24 -0300

    test: Added tests for download and extract GTFS files

---
+++ src/test/java/br/com/msp/busiq/infrastructure/gateway/GtfsGatewayImplTest.java
@@ -0,0 +1,64 @@
+package br.com.msp.busiq.infrastructure.gateway;
+
+import br.com.msp.busiq.core.gateway.GtfsGateway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class GtfsGatewayImplTest {
+    @Autowired
+    private GtfsGateway gtfsGateway;
+
+    @Value("${gtfs.destination-path}")
+    private String TEST_GTFS_PATH;
+
+    @BeforeEach
+    @AfterEach
+    void cleanTestGtfsDir() {
+        Path dir = Paths.get(TEST_GTFS_PATH);
+        if (Files.exists(dir)) {
+            try (Stream<Path> files = Files.walk(dir)) {
+                files
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(p -> {
+                            try { Files.delete(p); } catch (IOException e) {
+                                throw new RuntimeException("Erro ao deletar o arquivo " + p, e);
+                            }
+                        });
+            } catch (IOException e) {
+                throw new RuntimeException("Erro ao tentar entrar no diretório " + TEST_GTFS_PATH, e);
+            }
+        }
+    }
+
+    @Test
+    void downloadGtfsSuccess() {
+        assertTrue(Files.exists(gtfsGateway.download()));
+    }
+
+    @Test
+    void extractGtfsSuccess() {
+        Path path = gtfsGateway.download();
+        assertTrue(Files.exists(gtfsGateway.extract()));
+    }
+
+    @Test
+    void extractGtfsError() {
+        assertTrue(Files.exists(gtfsGateway.extract()));
+    }
+}
