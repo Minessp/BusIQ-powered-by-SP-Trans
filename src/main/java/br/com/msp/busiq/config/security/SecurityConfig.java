353ed05ad769188486905683a5f182dde978b766
Minessp <minestadionchannel@gmail.com>
2025-11-16 14:39:20 -0300

    refactor: Explicit state type added

--- src/main/java/br/com/msp/busiq/config/security/SecurityConfig.java
+++ src/main/java/br/com/msp/busiq/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,8 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, authenticatedEndpoints()).authenticated()
                         .anyRequest().permitAll());
