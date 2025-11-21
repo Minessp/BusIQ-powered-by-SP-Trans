53c05f1a678f24cd84d40f06f4e3f7b85468ab3c
Minessp <minestadionchannel@gmail.com>
2025-11-21 16:17:13 -0300

    feat: Added validation for fields when CreateUserRequest is created

--- src/main/java/br/com/msp/busiq/infrastructure/dtos/user/CreateUserRequest.java
+++ src/main/java/br/com/msp/busiq/infrastructure/dtos/user/CreateUserRequest.java
@@ -1,7 +1,20 @@
 package br.com.msp.busiq.infrastructure.dtos.user;
 
-public record CreateUserRequest(String name,
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CreateUserRequest(@NotBlank String name,
+                                @NotBlank
+                                @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+                                        flags = Pattern.Flag.CASE_INSENSITIVE, message = "Email inválido")
                                 String email,
+                                @NotBlank(message = "Senha é obrigatória")
+                                @Pattern(
+                                        regexp = "^(?=.*?\\p{Lu})(?=.*?\\p{Ll})(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+                                        message = "A senha deve ter no mínimo 8 caracteres, 1 letra maiúscula," +
+                                                " 1 letra minúscula, 1 número e 1 caractere especial"
+                                )
                                 String password,
                                 String role) {
 
